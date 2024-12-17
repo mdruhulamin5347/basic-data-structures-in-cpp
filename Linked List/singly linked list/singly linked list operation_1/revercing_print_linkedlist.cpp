4d6c08fc16b129fd6d37e55018920992faa89576
Md.Ruhul Amin <mdruhulamin534793@gmail.com>
2024-12-17 19:14:37 +0600

    implement revercing print data in linked list for singly linked list

---
+++ Linked List/singly linked list/singly linked list operation_1/revercing_print_linkedlist.cpp
@@ -0,0 +1,67 @@
+
+
+
+
+
+
+// Revercing print linkedlist
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Node{
+    public:
+    int val;
+    Node* next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node* &head, Node* &tail, int val)
+{
+    Node* newNode = new Node(val);
+    if(head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next = newNode;
+    tail = newNode; 
+}
+
+void reverce_print(Node* temp)
+{    
+    if(temp == NULL)
+    {
+        return;
+    }    
+    reverce_print(temp->next);
+    cout << temp->val << " ";
+}
+
+int main(){
+    Node* head = NULL;
+    Node* tail = NULL;
+
+    while(true)
+    {   
+        int val;
+        cin >> val;
+        if(val == -1)
+        {
+            break;
+        }        
+        insert_at_tail(head,tail,val);
+    }
+    
+
+    reverce_print(head);
+    return 0;
+    
+}
