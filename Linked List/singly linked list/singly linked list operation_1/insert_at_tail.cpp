4a800cde3e1c9951da8f7355b05dfb881805433a
Md.Ruhul Amin <mdruhulamin534793@gmail.com>
2024-12-17 18:13:09 +0600

    implement node and insert at tail for singly linked list

---
+++ Linked List/singly linked list/singly linked list operation_1/insert_at_tail.cpp
@@ -0,0 +1,53 @@
+
+
+
+// insert at tail
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Node{
+    public:
+    int val;
+    Node* next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node* temp, int val)
+{
+    Node* newNode = new Node(val);
+    while(temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+void node_data_print(Node* head)
+{
+    Node* temp = head;
+    while(temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+}
+
+int main(){
+    Node* head = new Node(20);
+    Node* a = new Node(30);
+    Node* b = new Node(40);
+
+    head->next = a;
+    a->next = b;
+    insert_at_tail(head,100);
+    insert_at_tail(head,200);
+    insert_at_tail(head,300);
+    node_data_print(head);
+    return 0;
+}
