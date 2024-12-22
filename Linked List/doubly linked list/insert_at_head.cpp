e711fce3a8c9e2af5e5908311ee1aaa9a8056ce0
Md.Ruhul Amin <mdruhulamin534793@gmail.com>
2024-12-22 21:09:17 +0600

    insert at head for doubly linkedlist

---
+++ Linked List/doubly linked list/insert_at_head.cpp
@@ -0,0 +1,74 @@
+
+
+// doubly linked list value insert at head
+
+
+
+
+
+// implementation and doubly linkedlist printing
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int val;
+    Node* prev;
+    Node* next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+void insert_at_head(Node* &head, Node* &tail,int val)
+{
+    Node* newNode = new Node(val);
+    if(head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    head->prev = newNode;
+    newNode->next = head;    
+    head = newNode;
+}
+
+void node_data_print_forward(Node* head)
+{
+    Node* temp = head;
+    while(temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main(){
+    Node* head = new Node(10);
+    Node* a = new Node(20);
+    Node* b = new Node(30);
+    Node* tail = new Node(40);
+
+    head->next = a;
+    a->prev = head;
+
+    a->next = b;
+    b->prev = a;
+
+    b->next = tail;
+    tail->prev = b;
+
+    insert_at_head(head,tail,100);
+    insert_at_head(head,tail,200);
+    node_data_print_forward(head);
+    
+    return 0;
+}
+
