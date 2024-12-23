88f646fe89f21bdc2e3a3af622cf6eb5684d73ee
Md.Ruhul Amin <mdruhulamin534793@gmail.com>
2024-12-23 15:47:05 +0600

    Singly Linked List Reverse

---
+++ Linked List/singly linked list/singly linked list operation_2/singly_linked_list_reverce.cpp
@@ -0,0 +1,77 @@
+
+
+
+// Singly linked list reversing using recursive funcion
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int val;
+    Node* next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+
+void insert_at_tail(Node* &head,Node* &tail, int val)
+{
+    Node* newNode = new Node(val);
+    if(head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next = newNode;
+    tail = newNode;
+}
+
+void reversing_linked_list(Node* &head,Node* &tail, Node* temp)
+{
+       if(temp->next == NULL)
+    {
+        head = temp;
+        return;
+    }
+    reversing_linked_list(head,tail,temp->next);
+    temp->next->next = temp;
+    temp->next = NULL;
+    tail = temp;
+}
+
+
+void node_data_print(Node* head)
+{
+    Node* temp = head;
+    while(temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+}
+
+int main(){
+    Node* head = NULL;
+    Node* tail = NULL;
+    int val;
+    while(true)
+    {
+        cin >> val;
+        if(val == -1)
+        {
+            break;
+        }
+        insert_at_tail(head,tail,val);
+    }
+    
+    reversing_linked_list(head,tail,head);
+    node_data_print(head);
+    return 0;
+}
