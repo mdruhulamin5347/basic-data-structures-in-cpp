f44d1903942da457f4d4204d91432e2ec404160d
Md.Ruhul Amin <mdruhulamin534793@gmail.com>
2025-01-03 17:41:03 +0600

    remember previous queries

--- Linked List/hackerrank problem solve/Remember_Previous_Queries.cpp
+++ Linked List/hackerrank problem solve/Remember_Previous_Queries.cpp
@@ -33,6 +33,41 @@
 using namespace std;
 
 int main(){
-    
+    list<int>l;
+
+    int q;
+    cin >> q;
+    while(q--)
+    {
+        int x,v;
+        cin >> x >> v;
+        
+        if(x==0)    
+            l.push_front(v);
+        else if(x==1)
+            l.push_back(v);
+        else if(x==2)
+        {
+            if (v >= 0 && v < l.size()) {
+                l.erase(next(l.begin(), v));
+            }        
+        }
+
+        cout << "L -> " ;
+        for(int val : l)
+        {
+            cout << val << " " ;
+        }
+        cout << endl;
+
+        cout << "R -> " ;
+        
+        for(auto it=l.rbegin(); it != l.rend(); it++)
+        {
+            cout << *it << " " ;
+        }
+        cout << endl;
+
+    }
     return 0;
 }
