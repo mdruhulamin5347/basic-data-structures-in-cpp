8561ec389419719039f0a0b4aef3259bcdc2be49
Md.Ruhul Amin <mdruhulamin534793@gmail.com>
2025-01-25 02:51:51 +0600

    STL priority-queue

--- STL priority_Queue, map, set/priority_queue.cpp
+++ STL priority_Queue, map, set/priority_queue.cpp
@@ -7,7 +7,8 @@
 using namespace std;
 
 int main(){
-    priority_queue<int>pq;
+    // priority_queue<int>pq; // descending order
+    priority_queue<int,vector<int>,greater<int>>pq;  // ascending order
     int val;
     while(cin >> val){
         pq.push(val);
