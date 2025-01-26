7cf48b986beac30aac44befd83eeb0cc5b6df4a3
Md.Ruhul Amin <mdruhulamin534793@gmail.com>
2025-01-26 16:03:50 +0600

    give current min

--- Problem contest for Data Structure/Give_Current_Min.cpp
+++ Problem contest for Data Structure/Give_Current_Min.cpp
@@ -29,6 +29,43 @@
 using namespace std;
 
 int main(){
-    
+    priority_queue<int,vector<int>,greater<int>>pq;
+    int n,q;
+    cin >> n;
+    while(n--){
+        int val;
+        cin >> val;
+        pq.push(val);
+    }
+
+    cin >> q;
+    while(q--){
+        int x;
+        cin >> x;
+        if(x==0){
+            int present;
+            cin >> present;
+            pq.push(present);
+            cout << pq.top() << endl;
+        }
+        else if(x==1){
+            if(pq.empty())
+                cout << "Empty" << endl;
+            else
+                cout << pq.top() << endl;
+        }
+        else if(x==2){
+            if(!pq.empty()) {
+                pq.pop();
+                if(pq.empty())
+                    cout << "Empty" << endl;
+                else
+                    cout << pq.top() << endl;
+            }
+            else {
+                cout << "Empty" << endl;
+            }
+        }        
+    }
     return 0;
 }
